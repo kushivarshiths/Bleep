1f8a559af9aa9b221675d39f816cc4b51e078678
dztrctstar <81169621+kushivarshiths@users.noreply.github.com>
2021-08-21 21:16:15 +0530

    Create bleep.cpp

---
+++ bleep.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+#include "functions.hpp"
+int main(){
+std::string word = "brocolli";
+std::string text = "i love brocolli Everyone Seems to like brocolli Some hate brocolli";
+bleep(text);
+std::cout << text;
+
+}
